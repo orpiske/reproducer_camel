07d2a68304361556e341bc1878f3711d73ce2d5c
Otavio Rodolfo Piske <angusyoung@gmail.com>
2025-01-27 10:34:23 +0100

    Disable tests

--- src/test/java/org/acme/GreetingResourceIT.java
+++ src/test/java/org/acme/GreetingResourceIT.java
@@ -1,7 +1,9 @@
 package org.acme;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import org.junit.jupiter.api.Disabled;
 
+@Disabled
 @QuarkusIntegrationTest
 class GreetingResourceIT extends GreetingResourceTest {
     // Execute the same tests but in packaged mode.
